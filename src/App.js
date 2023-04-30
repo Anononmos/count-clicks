839e39fceb9da1afb50143a5593a5232c7653aaf
Anononmos <ainan.kashif@gmail.com>
2023-04-30 02:10:13 -0400

    learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>Click Counter</h1>
+            <Counter i={0}></Counter>
+        </>
+    );
+}
+
+function Counter(i = 0) {
+    const [count, setCount] = useState(i);
+
+    return (
+        <button className='counter'>You clicked {count} times.</button>
+    );
 }
 
 export default App;
